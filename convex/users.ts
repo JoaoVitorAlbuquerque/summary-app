1df88767c453363388d74955920ba1f94dc0f630
JoaoVitorAlbuquerque <joaovitorparussoloalbuquerque@gmail.com>
2024-09-28 17:21:35 -0300

    fix: error typescript user-button

--- convex/users.ts
+++ convex/users.ts
@@ -1,10 +1,10 @@
-import { getAuthSessionId } from "@convex-dev/auth/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
 import { query } from "./_generated/server";
 
 export const current = query({
   args: {},
   handler: async (ctx) => {
-    const userId = await getAuthSessionId(ctx);
+    const userId = await getAuthUserId(ctx);
 
     if (userId === null) {
       return null;
