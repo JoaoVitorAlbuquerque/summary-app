97ee9b1219ee991e9bb9312a138132cf00376b84
JoaoVitorAlbuquerque <joaovitorparussoloalbuquerque@gmail.com>
2024-09-30 20:53:45 -0300

    feat: create workspace header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkSpaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -11,9 +18,28 @@ export default function WorkspaceIdLayout({ children }: WorkspaceIdLayoutProps) 
   return (
     <div className="h-full">
       <Toolbar />
+
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkSpaceSidebar />
+          </ResizablePanel>
+
+          <ResizableHandle withHandle />
+
+          <ResizablePanel minSize={20}>
+            {children}
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
