c5d884fd5b2d78f5b9cce3f190be47703eb8a410
JoaoVitorAlbuquerque <joaovitorparussoloalbuquerque@gmail.com>
2024-10-03 17:25:00 -0300

    feat: editor submit

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,10 @@
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -9,16 +13,51 @@ interface ChatInputProps {
 }
 
 export function ChatInput({ placeholder }: ChatInputProps) {
+const [editorKey, setEditorKey] = useState(0);
+const [isPending, setIsPending] = useState(false);
+
   const editorRef = useRef<Quill | null>(null);
+
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+
+  const { mutate: createMessage } = useCreateMessage();
+
+  async function handleSubmit({
+    body,
+    image
+  }: {
+    body: string;
+    image: File | null;
+  }) {
+    console.log({ body, image });
+    try {
+      setIsPending(true);
+
+      createMessage({
+        workspaceId,
+        channelId,
+        body,
+      }, {});
+
+      setEditorKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      toast.error("Erro ao enviar a mensagem");
+    } finally {
+      setIsPending(false);
+    }
+  }
 
   // editorRef.current?.focus();
 
   return (
     <div className="px-5 w-full">
       <Editor
-        onSubmit={() => {}}
+        key={editorKey}
+        onSubmit={handleSubmit}
         placeholder={placeholder}
-        disabled={false}
+        disabled={isPending}
+        innerRef={editorRef}
       />
     </div>
   );
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -7,14 +7,15 @@ import "quill/dist/quill.snow.css";
 import { Button } from "./ui/button";
 import { Hint } from "./hint";
 
-import { ImageIcon, Smile, FileIcon } from "lucide-react";
+import { ImageIcon, Smile, FileIcon, XIcon } from "lucide-react";
 import { MdSend } from "react-icons/md";
 import { Delta, Op } from "quill/core";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 type EditorValue = {
   image: File | null;
-  file: File | null;
+  file?: File | null;
   body: string;
 };
 
@@ -38,6 +39,7 @@ export default function Editor({
   variant = "create",
 }: EditorProps) {
   const [text, setText] = useState('');
+  const [image, setImage] = useState<File | null>(null);
   const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
   const containerRef = useRef<HTMLDivElement>(null);
@@ -47,6 +49,7 @@ export default function Editor({
   const quillRef = useRef<Quill | null>(null);
   const defaultValueRef = useRef(defaultValue);
   const disabledRef = useRef(disabled);
+  const imageElementRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
@@ -72,7 +75,15 @@ export default function Editor({
             enter: {
               key: "Enter",
               handler: () => {
-                return;
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                const isEmpty = !addedImage && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) return;
+
+                const body = JSON.stringify(quill.getContents());
+                submitRef.current?.({ body, image: addedImage });
               },
             },
             shift_enter: {
@@ -125,12 +136,48 @@ export default function Editor({
     }
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageElementRef}
+        onChange={(event) => setImage(event.target.files![0])}
+        className="hidden"
+      />
+
+      <div className={cn(
+        "flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white",
+        disabled && "opacity-50",
+        )}>
         <div ref={containerRef} className="h-full ql-custom" />
+
+        {!!image && (
+          <div className="p-2">
+            <div className="relative size-[62px] flex items-center justify-center group/image">
+              <Hint label="Remover imagem">
+                <button
+                  onClick={() => {
+                    setImage(null);
+                    imageElementRef.current!.value = "";
+                  }}
+                  className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                >
+                  <XIcon className="size-3.5" />
+                </button>
+              </Hint>
+
+              <Image
+                src={URL.createObjectURL(image)}
+                alt="Uploaded"
+                fill
+                className="rounded-xl overflow-hidden border object-cover"
+              />
+            </div>
+          </div>
+        )}
 
         <div className="flex px-2 pb-2 z-[5]">
           <Hint label={isToolbarVisible ? "Ocultar formatação" : "Mostrar formatação"}>
@@ -162,7 +209,7 @@ export default function Editor({
                   disabled={disabled}
                   size="iconSm"
                   variant="ghost"
-                  onClick={() => {}}
+                  onClick={() => imageElementRef.current?.click()}
                 >
                   <ImageIcon className="size-4" />
                 </Button>
@@ -186,7 +233,7 @@ export default function Editor({
               <Button
                 variant="outline"
                 size="sm"
-                onClick={() => {}}
+                onClick={onCancel}
                 disabled={disabled}
               >
                 Cancelar
@@ -195,7 +242,12 @@ export default function Editor({
               <Button
                 disabled={disabled || isEmpty}
                 size="sm"
-                onClick={() => {}}
+                onClick={() => {
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  });
+                }}
                 className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
               >
                 Salvar alterações
@@ -206,7 +258,12 @@ export default function Editor({
           {variant === "create" && (
             <Button
               disabled={disabled || isEmpty}
-              onClick={() => {}}
+              onClick={() => {
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                });
+              }}
               size="iconSm"
               className={cn(
                 "ml-auto",
@@ -221,11 +278,13 @@ export default function Editor({
         </div>
       </div>
 
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Enter</strong> para adicionar uma nova linha
-        </p>
-      </div>
+      {variant === "create" && (
+        <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+          <p>
+            <strong>Shift + Enter</strong> para adicionar uma nova linha
+          </p>
+        </div>
+      )}
     </div>
   );
 }
